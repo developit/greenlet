23301ccf42f153bbf877208a1ba45409907c9373
Sam Brown <sam@sgb.io>
2018-01-29 16:48:14 +0000

    Proper naming for things, some annotation

--- greenlet.js
+++ greenlet.js
@@ -1,25 +1,36 @@
 /** Move an async function into its own thread.
- *  @param {Function} fn	The (async) function to run in a Worker.
+ *  @param {Function} fn    The (async) function to run in a Worker.
  */
 export default function greenlet(fn) {
-	let w = new Worker(URL.createObjectURL(new Blob([
-			'onmessage=('+(
-				f => ({ data }) => Promise.resolve().then(
-					() => f.apply(f, data[1])
+	const onMessage = originalFn =>
+		({ data }) =>
+			// run it's original function f, with data[1] as arguments
+			// postMessage with d as 3rd arg if promise is successful
+			// otherwise post error as 2nd argument
+			Promise.resolve()
+				.then(
+					() => originalFn.apply(originalFn, data[1]),
 				).then(
-					d => { postMessage([data[0], null, d]); },
-					e => { postMessage([data[0], ''+e]); }
-				)
-			)+')('+fn+')'
-		]))),
-		c = 0,
-		p = {};
-	w.onmessage = ({ data: [c,e,d] }) => {
-		p[c][e?1:0](e||d);
-		delete p[c];
+					(childData) => { postMessage([data[0], null, childData]); },
+					(error) => { postMessage([data[0], '' + error ]); }
+				);
+	const blob = new Blob([`onmessage =${onMessage}(${fn})`]);
+
+	const worker = new Worker(URL.createObjectURL(blob));
+	let counter = 0;
+	const promise = {};
+
+	worker.onmessage = ({ data: [callback, error, data] }) => {
+		// promise callback for this message call
+		// if error, takes first array entry, otherwise second
+		// and passes error otherwise original data
+		promise[callback][error ? 1 : 0](error || data);
+		delete promise[callback];
 	};
-	return (...a) => new Promise( (y, n) => {
-		p[++c] = [y, n];
-		w.postMessage([c, a]);
-	});
+
+	return (...a) =>
+		new Promise((resolver, rejector) => {
+			promise[++counter] = [resolver, rejector];
+			worker.postMessage([counter, a]);
+		});
 }
