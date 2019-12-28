6d69e9a109e61efe0ceca864ab7492001cad079c
John Johnson II <johnsonjo4531@gmail.com>
2019-12-27 20:54:15 -0700

    Added Generators and Async Generators support
    fixes https://github.com/developit/greenlet/issues/35
    
    tldr; Mainly wrapped the existing promise api to get it to work
    with async generators.
    I took atleast two iterations to get to this point. At first I thought
    to do the job with a readable Stream implementing async iterable
    on the main thread, but then was afraid of inconsistencies that
    would arise between the two apis. For example Readable Stream
    when finished will only return `{ done: true, value: undefined }`
    whereas async iterables can return `{ done: true, value: any }`
    when `any` is any value.
    
    So, then I decided to make a async generator that could talk to
    the worker for better compatibility. One thing to note is that the worker
    data onmessage receives an extra piece for the status to cause the
    iterator to use. This is similar to the Promise status, but for
    generators.

--- greenlet.js
+++ greenlet.js
@@ -1,34 +1,69 @@
 /** Move an async function into its own thread.
- *  @param {Function} asyncFunction  An (async) function to run in a Worker.
+ *  @param {Function} asyncFunction  An (async) or async generator function to run in a Worker.
+ *  @param {{useTransferables?: boolean}} options
+ * 	useTransferables defaults to true.
  *  @public
  */
-export default function greenlet(asyncFunction) {
+export default function greenlet(asyncFunction, options = {}) {
+	const defaults = {
+		useTransferables: true
+	};
+	const { useTransferables } = { ...defaults, ...options };
 	// A simple counter is used to generate worker-global unique ID's for RPC:
-	let currentId = 0;
+	let promiseIds = 0;
+
+	// A simple counter is use to generate worker-global generator ID's for RPC:
+	let genIds = 0;
 
 	// Outward-facing promises store their "controllers" (`[request, reject]`) here:
 	const promises = {};
 
 	// Use a data URI for the worker's src. It inlines the target function and an RPC handler:
-	const script = '$$='+asyncFunction+';onmessage='+(e => {
-		/* global $$ */
-
-		// Invoking within then() captures exceptions in the supplied async function as rejections
-		Promise.resolve(e.data[1]).then(
-			v => $$.apply($$, v)
-		).then(
-			// success handler - callback(id, SUCCESS(0), result)
-			// if `d` is transferable transfer zero-copy
-			d => {
-				postMessage([e.data[0], 0, d], [d].filter(x => (
-					(x instanceof ArrayBuffer) ||
-					(x instanceof MessagePort) ||
-					(self.ImageBitmap && x instanceof ImageBitmap)
-				)));
-			},
-			// error handler - callback(id, ERROR(1), error)
-			er => { postMessage([e.data[0], 1, '' + er]); }
-		);
+	const script = `$$=${asyncFunction};USET=${useTransferables};GENS={};onmessage=` + (e => {
+		const getTransferables = d => !USET ? [] : d.filter(x => (
+			(x instanceof ArrayBuffer) ||
+			(x instanceof MessagePort) ||
+			(self.ImageBitmap && x instanceof ImageBitmap)
+		));
+		const [promiseID, args, status, genID] = e.data;
+		/* global $$, GENS, USET */
+		if ($$.constructor.name === 'AsyncGeneratorFunction' || $$.constructor.name === 'GeneratorFunction') {
+			Promise.resolve(args).then(
+				// either apply the generator function or use it's iterator
+				v => !GENS[genID] ? $$.apply($$, v) : GENS[genID][status](v[0])
+			).then(
+				// success handler - callback(id, SUCCESS(0))
+				// if `d` is transferable transfer zero-copy
+				d => {
+					// setup the generator
+					if (!GENS[genID]) {
+						GENS[genID] = [d.next.bind(d), d.return.bind(d), d.throw.bind(d)];
+						return postMessage([promiseID, 0, { value: undefined, done: false }]);
+					}
+					// yield the value
+					postMessage([promiseID, 0, d], getTransferables([d.value]));
+					if (d.done) {
+						GENS[promiseID] = null;
+					}
+				},
+				// error handler - callback(id, ERROR(1), error)
+				er => { postMessage([promiseID, 1, '' + er]); }
+			);
+		}
+		else {
+			// Invoking within then() captures exceptions in the supplied async function as rejections
+			Promise.resolve(args).then(
+				v => $$.apply($$, v)
+			).then(
+				// success handler - callback(id, SUCCESS(0), result)
+				// if `d` is transferable transfer zero-copy
+				d => {
+					postMessage([promiseID, 0, d], getTransferables([d]));
+				},
+				// error handler - callback(id, ERROR(1), error)
+				er => { postMessage([e.data[0], 1, '' + er]); }
+			);
+		}
 	});
 	const workerURL = URL.createObjectURL(new Blob([script]));
 	// Create an "inline" worker (1:1 at definition time)
@@ -48,20 +83,50 @@ export default function greenlet(asyncFunction) {
 		promises[e.data[0]] = null;
 	};
 
-	// Return a proxy function that forwards calls to the worker & returns a promise for the result.
-	return function (args) {
-		args = [].slice.call(arguments);
-		return new Promise(function () {
-			// Add the promise controller to the registry
-			promises[++currentId] = arguments;
+	const passMessagePromise = (args, status, genID) => new Promise(function () {
+		// Add the promise controller to the registry
+		promises[++promiseIds] = arguments;
 
-			// Send an RPC call to the worker - call(id, params)
-			// The filter is to provide a list of transferables to send zero-copy
-			worker.postMessage([currentId, args], args.filter(x => (
-				(x instanceof ArrayBuffer) ||
-				(x instanceof MessagePort) ||
-				(self.ImageBitmap && x instanceof ImageBitmap)
-			)));
-		});
+		// Send an RPC call to the worker - call(id, params)
+		// The filter is to provide a list of transferables to send zero-copy
+		worker.postMessage([promiseIds, args, status, genID], !useTransferables ? [] : args.filter(x => (
+			(x instanceof ArrayBuffer) ||
+			(x instanceof MessagePort) ||
+			(self.ImageBitmap && x instanceof ImageBitmap)
+		)));
+	});
+	// if it's a generator or async generator function return a async generator function.
+	if (asyncFunction.constructor.name === 'AsyncGeneratorFunction' || asyncFunction.constructor.name === 'GeneratorFunction') {
+		return async function* workerPassthrough(...args) {
+			const genID = ++genIds;
+			try {
+				let result = await passMessagePromise(args, 0, genID);
+				let value;
+				while (!result.done) {
+					// request next value
+					result = await passMessagePromise([value], 0, genID);
+					if (result.done) {
+						break;
+					}
+					value = yield result.value;
+				}
+				return result.value;
+			}
+			catch (err) {
+				// send error message
+				await passMessagePromise(['' + err], 2, genID);
+				throw err;
+			}
+			finally {
+				// send return message
+				await passMessagePromise([undefined], 1, genID);
+			}
+		};
+	}
+
+	// Return a proxy function that forwards calls to the worker & returns a promise for the result.
+	return function () {
+		const args = [].slice.call(arguments);
+		return passMessagePromise(args);
 	};
 }
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,15 @@
-type AsyncFunction<S extends any[], T> = (...args: S) => Promise<T>;
+interface Options {
+    useTransferables: boolean
+}
 
-type MaybeAsyncFunction<S extends any[], T> = (...args: S) => (T | Promise<T>);
+type AsyncGenFunction<S extends any[], T=unknown, TReturn=any, TNext=unknown> = (...args: S) => AsyncGenerator<T, TReturn, TNext>;
 
-export default function greenlet<S extends any[], T>(fn: MaybeAsyncFunction<S, T>): AsyncFunction<S, T>;
+type AsyncFunction<S extends any[], T=unknown> = (...args: S) => Promise<T>;
+
+type GenFunction<S extends any[], T=unknown, TReturn=any, TNext=unknown> = (...args: S) => (Generator<T, TReturn, TNext> | AsyncGenerator<T, TReturn, TNext>);
+
+type MaybeAsyncFunctionButNotGen<S extends any[], T=unknown, TReturn=any,TNext=unknown> = (...args: S) => (Exclude<T, Generator<T, TReturn, TNext> | AsyncGenerator<T, TReturn, TNext>> | Promise<T>);
+
+type GreenletFnType<S extends any[], T, TReturn = any, TNext = unknown> = MaybeAsyncFunctionButNotGen<S, T, TReturn, TNext> | GenFunction<S, T, TReturn, TNext>;
+
+export default function greenlet<S extends any[], T=unknown, TReturn=any, TNext=unknown, U extends GreenletFnType<S,T,TReturn,TNext> = GreenletFnType<S, T, TReturn, TNext>>(fn: U, options?: Options): U extends GenFunction<infer S,infer T,infer TReturn,infer TNext> ?  AsyncGenFunction<S, T, TReturn, TNext> : U extends MaybeAsyncFunctionButNotGen<infer S,infer T> ? AsyncFunction<S, T>  : never;
